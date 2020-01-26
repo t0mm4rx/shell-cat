d78dc3fe66821cfca05d9d15a02f686a99dbf98b
t0mm4rx <tom.marx@hotmail.fr>
2020-01-26 12:57:53 +0100

    check if zshrc and bashrc exist

--- ReadMe.md
+++ ReadMe.md
@@ -11,6 +11,11 @@ That simple utility creates two commands in your zsh or bash config :
 git clone https://github.com/t0mm4rx/shell-cat && cd shell-cat && python install.py && cd ../ && rm -rf shell-cat
 ```
 
+Don't forget to source the shell you use, or open a new terminal:
+```sh
+source ~/.zshrc
+```
+
 ## Problem ?
 
 Something went wrong ? Just delete your .bashrc and .zshrc and rename .bashrc_backup to .bashrc and .zshrc_backup to .zshrc.
--- install.py
+++ install.py
@@ -29,16 +29,18 @@ cat_fn = input("Enter the display cat function name :")
 
 if (not os.path.isdir("{}.cats".format(home))):
 	shutil.copytree("./cats", "{}.cats".format(home))
-shutil.copy("{}.zshrc".format(home), "{}.zshrc_backup".format(home))
-shutil.copy("{}.bashrc".format(home), "{}.bashrc_backup".format(home))
 
-with open("{}.bashrc".format(home), 'a') as file:
-	file.write('\n\n# Addition by shell-cat\n')
-	file.write(PUSH_FN.replace("$$$1", push_fn))
-	file.write("\n")
-	file.write(CAT_FN.replace("$$$1", cat_fn))
+if (os.path.exists("{}.bashrc".format(home))):
+	shutil.copy("{}.bashrc".format(home), "{}.bashrc_backup".format(home))
+	with open("{}.bashrc".format(home), 'a') as file:
+		file.write('\n\n# Addition by shell-cat\n')
+		file.write(PUSH_FN.replace("$$$1", push_fn))
+		file.write("\n")
+		file.write(CAT_FN.replace("$$$1", cat_fn))
 
-with open("{}.zshrc".format(home), 'a') as file:
+if (os.path.exists("{}.zshrc".format(home))):
+	shutil.copy("{}.zshrc".format(home), "{}.zshrc_backup".format(home))
+	with open("{}.zshrc".format(home), 'a') as file:
 	file.write('\n\n# Addition by shell-cat\n')
 	file.write(PUSH_FN.replace("$$$1", push_fn))
 	file.write("\n")
